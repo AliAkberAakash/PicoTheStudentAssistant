d0f4eb3557c89bd17c80315289f62818f4c782bc
ALIAKBERAAKASH <ali852609@gmail.com>
2018-09-15 10:57:45 +0600

    show current day in routine fixed

--- app/src/main/java/com/example/cedwa/studentassistant/Home/Routine/Routines.java
+++ app/src/main/java/com/example/cedwa/studentassistant/Home/Routine/Routines.java
@@ -9,7 +9,9 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.cedwa.studentassistant.DatabaseFiles.Config;
 import com.example.cedwa.studentassistant.Home.Routine.days.Friday;
 import com.example.cedwa.studentassistant.Home.Routine.days.Monday;
 import com.example.cedwa.studentassistant.Home.Routine.days.Saturday;
@@ -18,18 +20,42 @@ import com.example.cedwa.studentassistant.Home.Routine.days.Thursday;
 import com.example.cedwa.studentassistant.Home.Routine.days.Tuesday;
 import com.example.cedwa.studentassistant.Home.Routine.days.Wednesday;
 import com.example.cedwa.studentassistant.R;
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.Logger;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 
 public class Routines extends Fragment {
+
+    Map< String,Fragment> daysMap;
+    private ViewPager viewPager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        //create the map
+        daysMap = new HashMap< String,Fragment>();
+        daysMap.put(Config.DAY_SAT, new Saturday());
+        daysMap.put(Config.DAY_SUN, new Sunday());
+        daysMap.put(Config.DAY_MON, new Monday());
+        daysMap.put(Config.DAY_TUE, new Tuesday());
+        daysMap.put(Config.DAY_WED, new Wednesday());
+        daysMap.put(Config.DAY_THU, new Thursday());
+        daysMap.put(Config.DAY_FRI, new Friday());
+
+        Logger.addLogAdapter(new AndroidLogAdapter());
+
         View result = inflater.inflate(R.layout.fragment_routines, container, false);
         SectionPagerAdapter sectionPagerAdapter = new SectionPagerAdapter(getChildFragmentManager());
-        ViewPager viewPager = result.findViewById(R.id.pager);
+        viewPager = result.findViewById(R.id.pager);
+        /*viewPager.addOnPageChangeListener(new CircularViewPagerHandler(viewPager));*/
         viewPager.setAdapter(sectionPagerAdapter);
+        viewPager.setCurrentItem(setCurrentDay());
 
         return result;
     }
@@ -44,6 +70,7 @@ public class Routines extends Fragment {
         public Fragment getItem(int position) {
             switch (position)
             {
+
                 case 0:
                     return new Saturday();
                 case 1 :
@@ -58,7 +85,9 @@ public class Routines extends Fragment {
                     return new Thursday();
                 case 6:
                     return new Friday();
+
             }
+
             return null;
         }
 
@@ -66,6 +95,41 @@ public class Routines extends Fragment {
         public int getCount() {
             return 7;
         }
+    }
+
+    public int setCurrentDay()
+    {
+        int day;
+        //set current day
+        Calendar sCalendar = Calendar.getInstance();
+        String dayLongName = sCalendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.getDefault());
+
+        Logger.d(dayLongName+" "+Config.DAY_SAT);
+
+        switch(dayLongName.toLowerCase())
+        {
+            case Config.DAY_SAT:
+                return 0;
+
+            case Config.DAY_SUN:
+                return 1;
+
+            case Config.DAY_MON:
+               return 2;
+
+            case Config.DAY_TUE:
+               return 3;
+
+            case Config.DAY_WED:
+                return 4;
+
+            case Config.DAY_THU:
+                return 5;
+
+            default:
+                return 6;
+        }
+
     }
 
 }
